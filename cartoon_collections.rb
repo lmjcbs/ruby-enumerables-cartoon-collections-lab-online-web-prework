5d472e7c26e45b6f96d5cec2ee880fa87b8e75c5
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-09-30 13:49:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  array.each_with_index{|value, index| puts "#{index} #{value}"}
+  array.each_with_index{|value, index| puts "#{index + 1} #{value}"}
 end
 
 def summon_captain_planet# code an argument here
