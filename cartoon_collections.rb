d0a3aea72bbd65b10ce00696b7523dc7ff74c690
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-09-30 14:25:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(ingredients)
-  cheese_found = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
   ingredients.each_with_index { |value, index| return ingredients[index] if cheese_types.include?(value)}
-  cheese_found
+  nil
 end
